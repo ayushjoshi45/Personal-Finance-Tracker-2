78288a21e2ff028c41c5a286c9fa4bd78c59bcc2
ayushjoshi45 <ayushjoshi3725@gmail.com>
2026-06-27 15:05:55 +0530

    update the backend code

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/transaction": {
+        target: "https://personal-finance-tracker-backend-k1nl.onrender.com",
+        changeOrigin: true,
+      },
+      "/api/v1/user": {
+        target: "https://personal-finance-tracker-backend-k1nl.onrender.com",
+        changeOrigin: true,
+      },
+    },
+  },
+});
